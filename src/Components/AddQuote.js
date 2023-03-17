cea1899001ec77780ccbd148e20e96db8b68abc8
Subba Rayudu <rayudups666@gmail.com>
2023-03-17 19:37:11 +0530

    validations added

--- src/Components/AddQuote.js
+++ src/Components/AddQuote.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuoteForm from './QuoteForm';
 
 
 const AddQuote = (props) =>{
+    const [isSave,setIsSave]=useState(false)
     const {addQuote} = props;
     const formSubmission =(formData) =>{
         addQuote(formData);
+        setIsSave(true)
     }
     return(
         <div>
             <h2>Add Quote</h2>
-            <QuoteForm formSubmission={formSubmission}/>
+            <QuoteForm formSubmission={formSubmission} isSave={isSave}/>
         </div>
         
     )
--- src/Components/QuoteForm.js
+++ src/Components/QuoteForm.js
@@ -1,30 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 
 const QuoteForm =(props) =>{
-    const { formSubmission,id:slNo,name: author,body: quote,handleToggle } =props;
-    const [id,setId]=useState(slNo ? slNo : Number(new Date()))
+    const { formSubmission,id:sNo,name: author,body: quote,handleToggle,isSave } =props;
+    const [id,setId]=useState(sNo ? sNo : uuidv4())
     const [name,setName]=useState(author ? author : '');
     const [body,setBody]=useState(quote ? quote : '');
+    const [errors,setErrors]=useState({})
+
+    useEffect(() =>{
+        if(isSave){
+        setId(uuidv4())
+        setBody('')
+        setName('')
+        }
+    },[isSave])
 
     const handleSubmit=(e) =>{
-        e.preventDefault()
         const formData={
             id:id,
             name:name,
             body:body
         }
+        const valid=validation();
+        e.preventDefault()
+        if(valid){
         formSubmission(formData)
         if(handleToggle){
             handleToggle()
         }
-        setBody('')
-        setName('')
+        }
+    }
+    const validation = () => {
+        let error={};
+        let isValid=true;
+        if(!name){
+            error.name="Please Enter the Name";
+            isValid=false;
+        }
+        if(body.length<=0){
+            error.body="Please Enter the Body";
+            isValid=false;
+        }
+        setErrors(error);
+        return isValid;
     }
     const handleName=(e) =>{
         setName(e.target.value)
     }
-    const handleBoday=(e) =>{
+    const handleBody=(e) =>{
         setBody(e.target.value)
     }
     return(
@@ -32,8 +57,10 @@ const QuoteForm =(props) =>{
             <form onSubmit={handleSubmit}>
                 <label>Name</label><br />
                 <input type="text" value={name} onChange={handleName}/><br />
+                <p style={{color:"red"}}>{errors.name}</p>
                 <label>body</label><br />
-                <input type="textarea" value={body} onChange={handleBoday}/><br /><br />
+                <input type="textarea" value={body} onChange={handleBody}/><br /><br />
+                <p style={{color:"red"}}>{errors.body}</p>
                 <input type="submit" value="Save" />
             </form>
         </div>
